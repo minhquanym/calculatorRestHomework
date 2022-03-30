3f8aa9b3dc3fe57c89e472cf0a7eb1d2983f37ef
Minh Quan <minhquanym@gmail.com>
2022-03-30 08:46:32 +0700

    Add api

--- src/main/java/com/example/calculatorrest/CalculatorController.java
+++ src/main/java/com/example/calculatorrest/CalculatorController.java
@@ -16,4 +16,44 @@ public class CalculatorController {
     ) {
 		return new Calculation(num0, num1, operator);
 	}
+
+  @GetMapping("/add")
+  public Calculation calAdd(
+    @RequestParam(value = "num0") Double num0,
+    @RequestParam(value = "num1") Double num1
+  ) {
+    return new Calculation(num0, num1, "+");
+  }
+
+  @GetMapping("/sub")
+  public Calculation calSub(
+    @RequestParam(value = "num0") Double num0,
+    @RequestParam(value = "num1") Double num1
+  ) {
+    return new Calculation(num0, num1, "-");
+  }
+
+  @GetMapping("/mul")
+  public Calculation calMul(
+    @RequestParam(value = "num0") Double num0,
+    @RequestParam(value = "num1") Double num1
+  ) {
+    return new Calculation(num0, num1, "*");
+  }
+
+  @GetMapping("/div")
+  public Calculation calDiv(
+    @RequestParam(value = "num0") Double num0,
+    @RequestParam(value = "num1") Double num1
+  ) {
+    return new Calculation(num0, num1, "/");
+  }
+
+  @GetMapping("/pow")
+  public Calculation calPow(
+    @RequestParam(value = "num0") Double num0,
+    @RequestParam(value = "num1") Double num1
+  ) {
+    return new Calculation(num0, num1, "**");
+  }
 }
